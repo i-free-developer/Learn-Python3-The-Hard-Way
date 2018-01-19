637763c78123c7d84e680a6091bdac2ea9264d36
xiaohu pei <little.tiger@163.com>
2018-01-19 22:51:18 +0800

    update
    coding

---
+++ ex10.py
@@ -0,0 +1,15 @@
+# Exercise 10 What was that
+tabby_cat = "\tI'm tabbed in."
+persian_cat = "I'm split\non a line."
+backslash_cat = "I'm \\ a \\ cat."
+fac_cat = """
+I'll do a list:
+\t* Cat Food
+\t* Fishies
+\t* Catnip\n\t* Grass
+"""
+
+print(tabby_cat)
+print(persian_cat)
+print(backslash_cat)
+print(fac_cat)
