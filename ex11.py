8b4ef8fa98137e13988e60d1d4d5af22ce9c8756
xiaohu pei <little.tiger@163.com>
2018-01-19 23:16:53 +0800

    update
    coding

---
+++ ex11.py
@@ -0,0 +1,11 @@
+# Exercise 11 Asking Questions
+print("How old are you?")
+age = input # only input in python3, no raw_input
+print("How tall are you?")
+height = input
+print("How much do you weigh?")
+weight = input
+
+print("So, you'r %r old, %r tall and %r heavy." %(age, height, weight))
+
+bye = input("time to say goodbye~!")
