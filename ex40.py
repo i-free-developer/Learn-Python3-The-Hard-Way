5a39984333366648aeb44852d7de315ebd8743b6
xiaohu pei <little.tiger@163.com>
2018-01-21 22:06:17 +0800

    update
    coding

---
+++ ex40.py
@@ -0,0 +1,22 @@
+# Exercise 40 Modules Classes and Objects
+class Song(object):
+    # my first object
+    def __init__(self, lyrics):
+        self.lyrics = lyrics
+
+    def sing_me_a_song(self):
+        for line in self.lyrics:
+            print(line)
+happy_bday = Song([
+  'Happy birthday to you',
+  'I don\'t want to get used',
+  'So I\' stop right there'
+  ])
+bulls_on_parade = Song([
+  'They rally around the family',
+  'With pockets full of shells'
+  ])
+
+happy_bday.sing_me_a_song()
+
+bulls_on_parade.sing_me_a_song()
