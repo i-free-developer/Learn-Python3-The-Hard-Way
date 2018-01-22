0192f1f51dd6343a1e47cc645bcc9e682f797dbe
xiaohu pei <little.tiger@163.com>
2018-01-22 11:22:52 +0800

    update
    finally, the code works in python3

--- ex41.py
+++ ex41.py
@@ -1,6 +1,6 @@
 # Exercise 41 Learning to Speak Object Oriented
 import random
-from urllib import urlopen
+from urllib import request
 import sys
 word_url = "http://learncodethehardway.org/words.txt"
 words = []
@@ -26,7 +26,8 @@ if len(sys.argv) == 2 and sys.argv[1] == "english":
     phrase_first = True
 
 # load up the words from the website
-for word in urlopen(word_url).readlines()
+for word in request.urlopen(word_url).readlines():
+    #word.decode()
     words.append(word.strip())
 
 
@@ -37,32 +38,48 @@ def convert(snippet, phrase):
     param_names =[]
 
 
-for i in range(0, snippet.count("@@@")):
-    param_count = random.randit(1,3)
-    param_names.append(', '.join(random.sample(words, param_count)))
+    for i in range(0, snippet.count("@@@")):
+        param_count = random.randit(1,3)
+        param_names.append(', '.join(random.sample(words, param_count)))
 
-for sentence in snippet, phrase:
-    result = sentence[:]
+    for sentence in snippet, phrase:
+        result = sentence[:]
 
-    # Fake class names
-    for word in class_names:
-        result = result.replace("%%%", word, 1)
+        # Fake class names
+        for word in class_names:
+            result = result.replace("%%%", word, 1)
 
-    # fake other names
-    for word in other_names:
-        result = result.raplace("***", word, 1)
+        # fake other names
+        for word in other_names:
+            result = result.replace("***", word, 1)
 
-    # fake parameter list
-    for word in param_names:
-        result = result.replace("@@@", word, 1)
+        # fake parameter list
+        for word in param_names:
+            result = result.replace("@@@", word, 1)
 
-    results.append(result)
+        results.append(result)
 
-return results
+    return results
 
 
 # keep going until they hit CTRL -D
+try:
+    while True:
+        snippets = list(phrases.keys())
+        random.shuffle(snippets)
 
+        for snippet in snippets:
+            phrase = phrases[snippet]
+            question, answer = snippet, phrase
+            if phrase_first:
+                question, answer = answer, question
+
+            print(question)
+
+            input("> ")
+            print("ANSWER: %s\n\n" %answer)
+except EOFError:
+  print("\nBye")
 
 
 
